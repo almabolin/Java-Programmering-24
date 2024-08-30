70981ec0f079161c3d5e371ae57a7320523e068a
almabolin <alma.bolin@live.com>
2024-08-30 13:57:28 +0200

    Created playground

---
+++ Java_playground/Java_playground.1/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main (String[] args) {
+            System.out.println( "Hello! What is your name? ");
+            Scanner s = new Scanner(System.in);
+            name = s.nextLine()
+            System.out.println(name);
+    }
+}
