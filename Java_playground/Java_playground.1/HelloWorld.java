158d938ef9abb9013fd5ba4756237e2d0324a737
almabolin <alma.bolin@live.com>
2024-08-30 14:34:36 +0200

    added ages

--- Java_playground/Java_playground.1/HelloWorld.java
+++ Java_playground/Java_playground.1/HelloWorld.java
@@ -5,7 +5,21 @@ public class HelloWorld {
             Scanner s = new Scanner(System.in);
             String name = "";
             name = s.nextLine();
-            System.out.print("Hello " + name);
+            System.out.println("Hello " + name);
+            System.out.println("How old are you? ");
+            int age = 0;
+            age = s.nextInt();
+            System.out.println("You are " + age + " years old!");
+            if (age <= 25 ) {
+                System.out.println ("You are 25 years old or younger.");
+            }
+            else if (age>25) 
+            {
+                System.out.println ("You are older than 25");
+            }
+            int number = 10;
+            int sum = age + number;
+            System.out.println("In 10 years you will be: " + sum);
             s.close();
     }
 }
