d8c7f136debe2fa295decade09b336a7649fa461
almabolin <alma.bolin@live.com>
2024-08-30 14:19:26 +0200

    Added code

--- Java_playground/Java_playground.1/HelloWorld.java
+++ Java_playground/Java_playground.1/HelloWorld.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class HelloWorld {
     public static void main (String[] args) {
-            System.out.println( "Hello! What is your name? ");
+            System.out.println("Hello! What is your name? ");
             Scanner s = new Scanner(System.in);
-            name = s.nextLine()
-            System.out.println(name);
+            String name = "";
+            name = s.nextLine();
+            System.out.print("Hello " + name);
+            s.close();
     }
 }
