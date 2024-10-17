cbfb14087a7042ad8a7ec00296d67784e3b26f31
almabolin <alma.bolin@live.com>
2024-10-17 14:33:43 +0200

    new excerises

---
+++ AlmaExercises/Exercises5/Arrays.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Arrays{
+    public int getInput(int x,int y){
+  
+        return x;
+       }
+
+    public static void main(String[] args) {
+    Scanner s = new Scanner(System.in); 
+    int[] randomInt;
+    randomInt = new int[5];
+    randomInt[0] = 2;
+    randomInt[1] = 4;
+    randomInt[2] = 5;
+    randomInt[3] = 7;
+    randomInt[4] = 9;
+   for (int element : randomInt) {
+    System.out.print(element);
+   }
+
+ 
+
+
+
+
+
+
+
+} 
+
+}
